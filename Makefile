28baa81e22b82bebf11d8f1438e3cd0061239d25
Joels <rozkalnsjoels@gmail.com>
2025-04-02 18:31:22 +0300

    First compilation that was sucsessful. os-image.bin is the compiled bootloader with the dummy kernel working. The kernel adds an 'X' on the top left corner of the screen.

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 all: run
 
 kernel.bin: kernel-entry.o kernel.o
-	ld -m elf_i386 -o $@ -Ttext 0x1000 $^ --oformat binary
+	i686-linux-gnu-ld -o $@ -Ttext 0x1000 $^ --oformat binary --entry=_start
 
 kernel-entry.o: kernel-entry.asm
 	nasm $< -f elf -o $@
 
 kernel.o: kernel.c
-	gcc -m32 -ffreestanding -c $< -o $@
+	gcc -m32 -ffreestanding -fno-pie -no-pie -c $< -o $@
 
 mbr.bin: mbr.asm
 	nasm $< -f bin -o $@
--- kernel-entry.asm
+++ kernel-entry.asm
@@ -1,4 +1,18 @@
-[bits 32]
-[extern main]
-call main
-jmp $
+[BITS 32]        ; We're in 32-bit mode
+[GLOBAL _start]  ; Make _start visible to the linker
+[EXTERN main]    ; Declare the C main function
+
+section .text
+_start:
+    ; Set up a stack (optional but recommended)
+    mov esp, stack_top  
+
+    ; Call main function in kernel.c
+    call main
+
+    ; Hang the CPU (halt forever)
+    jmp $
+
+section .bss
+resb 4096         ; Reserve 4KB for stack
+stack_top:
